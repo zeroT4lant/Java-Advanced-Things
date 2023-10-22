691cf7efef5c30b1664b6d856bac1fa8377715bd
Semeon Fedoseev <fadeevsemeon@yandex.ru>
2023-10-22 22:19:33 +0400

    13/69 done

---
+++ src/main/java/HashMap/HashMapEx1.java
@@ -0,0 +1,49 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEx1 {
+    public static void main(String[] args) {
+        HashMap<String,Integer> obshaga1 = new HashMap<>();
+        obshaga1.put("Andrew Kondratyev",405);
+        obshaga1.put("Kirik",405);
+        obshaga1.put("Dinar",405);
+        System.out.println(obshaga1.get("Andrew Kondratyev"));
+        System.out.println(obshaga1.keySet());
+        System.out.println(obshaga1.values());
+        System.out.println(obshaga1.containsKey("Kirik"));
+        System.out.println(obshaga1.containsValue(405));
+        System.out.println(obshaga1.isEmpty());
+        for (Map.Entry<String,Integer> e : obshaga1.entrySet()) {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+
+        HashMap<String,Integer> obshaga = new HashMap<>();
+        obshaga.put("Botinok",407);
+        obshaga.put("I am",407);
+        obshaga.put("Maksos",407);
+        //добавь если такого нет, проверка по ключу
+        obshaga.putIfAbsent("Maksim",405);
+        System.out.println(obshaga);
+
+        System.out.println(obshaga.get("I am"));
+
+
+        obshaga.remove("Botinok");
+        System.out.println(obshaga);
+
+        System.out.println(obshaga.containsValue(405));
+
+        System.out.println(obshaga.keySet());
+        System.out.println(obshaga.values());
+
+        for (Map.Entry<String,Integer> e : obshaga.entrySet()){
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+    }
+}
